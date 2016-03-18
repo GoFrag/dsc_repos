352924aa0d0aacbd9b8dd1579142f8f003506c77
dlhansford <davhans@microsoft.com>
2016-03-18 16:07:50 -0400

    Added files via upload

---
+++ NewWebServer.ps1
@@ -0,0 +1,23 @@
+﻿cofiguration CNYWebServer {
+    param
+    (
+        # Target nodes to apply the configuration
+        [string[]]$NodeName = 'localhost'
+)
+    Node $NodeName
+    {
+        # Install the IIS role
+        WindowsFeature IIS
+        {
+            Ensure          = "Present"
+            Name            = "Web-Server"
+        }
+
+        # Install the ASP .NET 4.5 role
+        WindowsFeature AspNet45
+        {
+            Ensure          = "Present"
+            Name            = "Web-Asp-Net45"
+        }
+     }
+}
